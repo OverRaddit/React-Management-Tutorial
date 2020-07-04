35accbebd2f9f0ba1b416f26958db751162cdee4
overReddit <kenwooshim@gmail.com>
2020-07-04 19:27:53 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '심건우',
+  'birthday' : '970514',
+  'gender': '남자',
+  'job' : '대학생'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : '홍길동',
+    'birthday' : '961222',
+    'gender': '남자',
+    'job' : '프로그래머'
+  },
+  {
+      'id' : 3,
+      'image' : 'https://placeimg.com/64/64/3',
+      'name' : '김채영',
+      'birthday' : '001215',
+      'gender': '여자',
+      'job' : '디자이너'
+  }
+]
+
+class App extends Component  {
+  render(){
   return (
-    <div className="gray-background">
-       <img src={logo} lat="logo" />
-       <h2>Let's develop management system!</h2>
+    <div>
+      {
+        customers.map(c =>{
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.brithday}
+              gender={c.gender}
+              job={c.job}
+              />
+          );
+        })
+      }
     </div>
-  );
+    );
+  }
 }
 
 export default App;
